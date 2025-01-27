d6f61bc45003bc2dcf2f86ab51829c66e22dd3d7
Hugh Toomey <hughtoomey@gmail.com>
2025-01-27 04:14:47 +0000

    hello world

---
+++ hello_423/target/debug/deps/hello_423-6f6bff710a169a90.d
@@ -0,0 +1,5 @@
+/workspaces/comp423-rust-tutorial/hello_423/target/debug/deps/hello_423-6f6bff710a169a90: src/main.rs
+
+/workspaces/comp423-rust-tutorial/hello_423/target/debug/deps/hello_423-6f6bff710a169a90.d: src/main.rs
+
+src/main.rs:
---
+++ hello_423/target/debug/hello_423.d
@@ -0,0 +1,1 @@
+/workspaces/comp423-rust-tutorial/hello_423/target/debug/hello_423: /workspaces/comp423-rust-tutorial/hello_423/src/main.rs
